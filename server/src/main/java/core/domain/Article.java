0e1834c409a0c39ecc06a936c0e9ec1327d592f0
julia <dev.julia.chae@gmail.com>
2025-01-15 18:49:00 +0900

    [S] Article 도메인을 블로그 형식에 맞춰서 수정

--- server/src/main/java/core/domain/Article.java
+++ server/src/main/java/core/domain/Article.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,27 +19,36 @@ public class Article {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  
+
   @Column(name = "id", updatable = false)
   private Long id;
-
 
   @Column(name = "title", nullable = false)
   private String title;
 
-  @Column(name = "content", nullable = false)
+  @Column(name = "subtitle")
+  private String subtitle;
+
+  @Column(name = "content", nullable = false, columnDefinition = "LONGTEXT")
   private String content;
 
+  @Column(name = "createdDate")
+  private LocalDateTime createdDate;
+
+  @Column(name = "category")
+  private String category;
+
+  @Column(name = "tags", columnDefinition = "LONGTEXT")
+  private String tags;
+
   @Builder
-  public Article(String title, String content) {
+  public Article(String title, String content, String subtitle, String category, String tags
+  ) {
     this.title = title;
     this.content = content;
+    this.subtitle = subtitle;
+    this.category = category != null ? category : "ect";
+    this.tags = tags;
+    this.createdDate = LocalDateTime.now();
   }
-
-  public void update(String title, String content) {
-    this.title = title;
-    this.content = content;
-  }
-
-
 }
