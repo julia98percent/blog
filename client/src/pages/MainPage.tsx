bf07c04e8c09360afd1db77d85d417f56a80f20d
julia <dev.julia.chae@gmail.com>
2025-01-19 02:09:51 +0900

    [C] MainPage 컴포넌트의 더미 데이터를 실제 api로 교체, 스타일 수정

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,34 +1,46 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import ArticleList from '@components/ArticleList';
+import type { ArticleCardType } from '@components/ArticleList';
+import { getArticleCardListFromCategory } from '../apis/get';
+import { useParams } from 'react-router-dom';
+
+const KOR_CATEGORY_NAME = {
+  undefined: '전체 글',
+  client: '클라이언트',
+  server: '서버',
+  'problem-solving': '문제풀이',
+  ect: '기타',
+};
 
 const MainPage = () => {
-  const [list, setList] = useState(null);
-
-  const getAllArticles = () => {
-    const response = axios
-      .get(import.meta.env.VITE_SERVER_URL + '/articles')
-      .then((item) => item?.data);
-    return response;
-  };
+  const { category } = useParams();
+  const [list, setList] = useState<null | ArticleCardType[]>(null);
 
   useEffect(() => {
-    getAllArticles().then((item) => {
-      setList(item);
-    });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const data = await getArticleCardListFromCategory(category);
+        setList(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [category]);
 
   if (list == null) {
     return '로딩 중';
   }
 
   return (
-    <div className="flex flex-col items-center">
-      {list.map((item, idx) => (
-        <div key={'wrapper-' + idx}>
-          <div key={'title-' + idx}>{item.title}</div>
-          <div key={'content-' + idx}>{item.content}</div>
-        </div>
-      ))}
+    <div className="flex flex-col items-center ">
+      <div className="mt-4 md:w-[720px] w-[80vw]">
+        <p className="text-xl font-light mb-1">
+          {KOR_CATEGORY_NAME[category]} ({list.length})
+        </p>
+        <ArticleList metadataList={list} />
+      </div>
     </div>
   );
 };
