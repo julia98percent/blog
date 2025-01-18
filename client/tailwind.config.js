45077642049f39cf1b9a8b564f187307e6e7377b
julia <dev.julia.chae@gmail.com>
2025-01-19 02:00:15 +0900

    [C] 컬러코드 추가, typography 플러그인 사용할 수 있도록 코드 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-font": "#202020",
+        "light-bg": "#f7f6f0",
+        "dark-font": "#e2e2e2",
+        "dark-divide": "#fe8c59",
+        "dark-bg": "#232221",
+        "divide-orange": "#e96724",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
