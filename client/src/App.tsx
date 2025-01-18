9ae7b0a769c47a9854e0a6eed6715b55ec9f5b60
julia <dev.julia.chae@gmail.com>
2025-01-19 02:06:10 +0900

    [C] 페이지 표출을 위한 라우트 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import MainPage from '@pages/MainPage';
 import AboutMePage from '@pages/AboutMePage';
 import { Routes, Route } from 'react-router-dom';
-import NavigationBar from '@components/NavigationBar';
+import NaviBar from '@components/NaviBar';
+import ArticlePage from '@pages/ArticlePage';
 
 function App() {
   return (
     <>
-      <NavigationBar />
+      <NaviBar />
+
       <Routes>
+        {/* for home page routing*/}
         <Route
           path="/"
           element={
@@ -16,6 +19,15 @@ function App() {
             </div>
           }
         />
+        <Route
+          path=":category"
+          element={
+            <div className="p-8 pt-0 w-screen max-w-screen-lg mx-auto my-0 h-full">
+              <MainPage />
+            </div>
+          }
+        />
+        <Route path="/post/:id" element={<ArticlePage />} />
         <Route
           path="/about-me"
           element={
