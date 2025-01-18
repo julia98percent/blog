f58d9133d1ca82864728b772c5b6931e71d190c4
julia <dev.julia.chae@gmail.com>
2025-01-19 02:08:29 +0900

    [C] 게시물 목록 표출을 위한 ArticleList 컴포넌트 추가

---
+++ client/src/components/ArticleList.tsx
@@ -0,0 +1,30 @@
+import { formatToYYYYMMDD } from '../utils/date';
+import { Link } from 'react-router-dom';
+
+export type ArticleCardType = {
+  id: string;
+  title: string;
+  createdAt: Date;
+  subtitle: string | null;
+  category: string;
+};
+
+const ArticleList = ({ metadataList }: { metadataList: ArticleCardType[] }) => {
+  return (
+    <div className="flex flex-col divide-y divide-divide-orange dark:divide-divide-orange/40 gap-1">
+      {metadataList.map(({ id, title, subtitle, createdAt }) => {
+        return (
+          <Link to={`/post/${id}`}>
+            <div>
+              <div className="mt-2">{formatToYYYYMMDD(createdAt)}</div>
+              <h2>{title}</h2>
+              <h4 className="mb-2 text-gray-600">{subtitle}</h4>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ArticleList;
